1532e5da5fb0502fa4a78e830bd3c904c3982322
felix-wag <felix.wagener@mediengruppe-rtl.de>
2021-08-17 11:43:13 +0200

    add ci pipeline - change script login

--- .github/scripts/docker_build_and_push.sh
+++ .github/scripts/docker_build_and_push.sh
@@ -4,13 +4,8 @@ set -e
 
 export IMAGE_NAME="${REGISTRY}/${REPOSITORY}/${BRANCH_REF}"
 
-# Authentications
-eval $(ssh-agent)
-ssh-add
-echo "${LOGIN_SECRET}" | docker login -u "${LOGIN_USER}" --password-stdin https://${REGISTRY}
-
 # Build and push
-docker build --ssh default $(./ci_helpers/get_docker_build_args.sh ${IMAGE_FOLDER}) -f ${DOCKERFILE:="./Dockerfile"} -t ${IMAGE_NAME}:${IMAGE_TAG} .
+docker build $(./ci_helpers/get_docker_build_args.sh ${IMAGE_FOLDER}) -f ${DOCKERFILE:="./Dockerfile"} -t ${IMAGE_NAME}:${IMAGE_TAG} .
 docker push ${IMAGE_NAME}:${IMAGE_TAG}
 
 # Push with alternative tag if present
